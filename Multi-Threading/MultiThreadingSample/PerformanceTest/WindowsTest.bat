e1c92709fbef38a918d077165a761cebfb91bbf6
Michael McKeough <mjm@datalogics.com>
2017-07-24 09:51:53 -0500

    Correct script and spelling.

---
+++ Multi-Threading/MultiThreadingSample/PerformanceTest/WindowsTest.bat
@@ -0,0 +1,14 @@
+cd ..
+..\x64\Release\MultiThreadingSample.exe .\PerformanceTest\Render_Pamper.txt
+..\x64\Release\MultiThreadingSample.exe .\PerformanceTest\Renderer_PDFRef.txt
+..\x64\Release\MultiThreadingSample.exe .\PerformanceTest\SF38718_44X34.txt
+..\x64\Release\MultiThreadingSample.exe .\PerformanceTest\Render_jmockit.txt
+..\x64\Release\MultiThreadingSample.exe .\PerformanceTest\Render_HorseMesa.txt
+..\x64\Release\MultiThreadingSample.exe .\PerformanceTest\SF38718_Problem.txt
+..\x64\Release\MultiThreadingSample.exe .\PerformanceTest\SF38650_Test_01.txt
+..\x64\Release\MultiThreadingSample.exe .\PerformanceTest\SF36923_RasterizeDoc.txt
+..\x64\Release\MultiThreadingSample.exe .\PerformanceTest\SF37312_AccessPages.txt.txt
+..\x64\Release\MultiThreadingSample.exe .\PerformanceTest\PDFa.txt
+..\x64\Release\MultiThreadingSample.exe .\PerformanceTest\PDFx.txt
+..\x64\Release\MultiThreadingSample.exe .\PerformanceTest\XPS2PDF.txt
+
